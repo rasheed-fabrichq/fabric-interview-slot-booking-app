48db25add0e710ebde5e8ad6ff8ccdebd8d6ce72
rasheed-aidetic <abdul.rasheed@aidetic.in>
2025-11-18 20:44:40 +0530

    Fix job ID for Senior Associate – Business Management in configuration

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ Configuration file for Multi-Job Interview Slot Booking Application
 JOBS = {
     'e34e8e92-95ff-47f5-8e2f-86baa397c2a0': 'Software Development Engineer I',
     'd770069d-de27-4489-bdcc-0122ebf68a05': 'Data Scientist – I',
-    '62566b89-8826-4140-8427-5413e4fa3ec7': 'Senior Associate – Business Management'
+    'a6c802c5-4861-4003-98c2-451069dff950': 'Senior Associate – Business Management'
 }
 
 def is_valid_job_id(job_id):
