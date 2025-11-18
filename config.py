ef7a1b726c6c5136a0d3e41d44e9cf0204288e10
rasheed-aidetic <abdul.rasheed@aidetic.in>
2025-11-18 20:47:20 +0530

    Update job definitions in configuration file for consistency

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ Configuration file for Multi-Job Interview Slot Booking Application
 
 # Job definitions (actual job UUIDs and names)
 JOBS = {
-    'e34e8e92-95ff-47f5-8e2f-86baa397c2a0': 'Software Development Engineer I',
-    'd770069d-de27-4489-bdcc-0122ebf68a05': 'Data Scientist – I',
+    'fc4c9c14-208c-427a-be2e-4d0080f286d6': 'Software Development Engineer I',
+    'b4f5ea74-a44a-4c93-a0f2-08abfaa337ed': 'Data Scientist – I',
     'a6c802c5-4861-4003-98c2-451069dff950': 'Senior Associate – Business Management'
 }
+
 
 def is_valid_job_id(job_id):
     """Check if job_id is valid"""
